c6ef80ee305a210e1a7aaa284634cf66191f8dc6
unknown <bocalist@gmail.com>
2019-08-21 16:07:06 +0900

    gogo gadget tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,15 +6,17 @@
     <div class="flex items-center mb-3">
         <a href="/projects/create">New Project</a>
     </div>
-    
-    <ul>
+
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
+            <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px;">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            </div>
         @empty
-            <li>No Projects Yet.</li>
+            <div>No Projects Yet.</div>
         @endforelse
-    </ul>
+    </div>
 
 @endsection
