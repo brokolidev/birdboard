4d9a38f73956c2d856e3b711d0b94dbe63312265
unknown <bocalist@gmail.com>
2019-08-21 10:43:16 +0900

    Scoping Projects

--- tests/Feature/projectsTest.php
+++ tests/Feature/projectsTest.php
@@ -11,11 +11,24 @@ class projectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
-    public function only_authenticated_users_can_create_projects()
+    public function guests_cannot_create_projects()
     {
         // $this->withoutExceptionHandling();
         $attributes = factory('App\Project')->raw();
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_a_single_project()
+    {
+        $project = factory('App\Project')->create();
+        $this->get($project->path())->assertRedirect('login');
     }
 
     /** @test */
@@ -53,15 +66,25 @@ class projectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_project()
+    public function a_user_can_view_there_project()
     {
+        $this->be(factory('App\User')->create());
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_project_of_others()
+    {
+        $this->be(factory('App\User')->create());
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     
