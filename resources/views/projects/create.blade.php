cfde409f834cb3cddb01e84f1fccadfb97da2fda
brokoli <bocalist@gmail.com>
2019-08-29 22:31:12 +0900

    prepping the frontend

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,46 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css" />
-    <title>Birdboard</title>
-</head>
+@section('content')
 
-<body>
+<form method="POST" action="/projects">
+    @csrf
+    <h1 class="heading is-1">Create a Project</h1>
 
-    <form method="POST" action="/projects" class="container">
-        @csrf
-        <h1 class="heading is-1">Create a Project</h1>
+    <div class="field">
+        <label class="label">Title</label>
+        <p class="control">
+            <input class="input" type="text" name="title" placeholder="Title">
+        </p>
+    </div>
 
-        <div class="field">
-            <label class="label">Title</label>
-            <p class="control">
-                <input class="input" type="text" name="title" placeholder="Title">
-            </p>
-        </div>
+    <div class="field">
+        <label class="label" for="description">Description</label>
+        <p class="control">
+            <textarea class="textarea" name="description" placeholder="Description"></textarea>
+        </p>
+    </div>
 
-        <div class="field">
-            <label class="label" for="description">Description</label>
-            <p class="control">
-                <textarea class="textarea" name="description" placeholder="Description"></textarea>
-            </p>
-        </div>
+    <div class="field is-grouped">
+        <p class="control">
+            <button type="submit" class="button is-link">Create Project</button>
+            <a href="/projects">Cancel</a>
+        </p>
+    </div>
+</form>
 
-        <div class="field is-grouped">
-            <p class="control">
-                <button type="submit" class="button is-link">Create Project</button>
-            </p>
-            <p class="control">
-                <button class="button is-danger">Cancel</button>
-            </p>
-        </div>
-    </form>
-
-
-
-</body>
-
-</html>
+@endsection
